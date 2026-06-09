be306933fbd06e4656afe437546e65c46c00ba55
Ayushi Prajapati <ayushi016750@gmail.com>
2026-06-09 13:01:49 +0530

    Implemented loops, methods and array in java

---
+++ array/array1d.java
@@ -0,0 +1,23 @@
+
+
+public class array1d {
+    public static void main(String args[]){
+       
+        int arr[] = {4,56,22,7,9};
+        arr[2]=88;
+
+        for(int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+        
+                
+                
+        int a[] = new int[4];  //a = {0,0,0,0}
+        a[0] = 12;
+        a[1] = 9;
+        a[2] = 5;
+        for(int i=0;i<4;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ array/array2d.java
@@ -0,0 +1,30 @@
+
+
+public class array2d {
+    public static void main(String[] args) {
+       int arr[][] = new int[3][4];
+       for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            arr[i][j] = (int) (Math.random()*10);
+        }
+       } 
+
+       for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            int a = arr[i][j];
+            System.out.print(a + " ");
+        }
+        System.out.println();
+       }
+       System.out.println("printing using enhanced for loop:");
+
+       for(int n[]: arr){
+        for(int m : n){
+            System.out.print(m + " ");
+        }
+        System.out.println();
+       }
+    }
+
+    
+}
