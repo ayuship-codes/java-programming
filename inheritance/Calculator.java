9974d2d9cf477e23be80c54d3d132ee9389fc436
Ayushi Prajapati <ayushi016750@gmail.com>
2026-06-11 13:22:36 +0530

    Implemented inheritance in java

---
+++ inheritance/Calculator.java
@@ -0,0 +1,13 @@
+
+
+public class Calculator {
+    
+    public static void main(String[] args) {
+        AdvCalc obj1 = new AdvCalc();
+        System.out.println(obj1.add(12,4));
+        System.out.println(obj1.sub(12,4));
+        System.out.println(obj1.mul(12,4));
+        System.out.println(obj1.div(12,4));
+    }
+}
+
